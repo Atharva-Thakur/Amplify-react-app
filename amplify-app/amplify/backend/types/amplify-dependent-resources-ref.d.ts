255949d616625f5e3485668426869c9e6e8a30bf
Atharva Thakur <atharvathakur369@gmail.com>
2023-07-15 22:06:49 +0530

    added graphQL and storage

--- amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactappampli": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactappampli70e3d3b6": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s32442b987": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
